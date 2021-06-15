5a08b2dcacacfb2763059ed43a9154e6e79c33f6
F Aurelio <onceacurious@gmail.com>
2021-06-15 16:12:11 +0800

    Update Feature Section
    Update layout and css of the section

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,11 +4,17 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const toggler = document.getElementById("toggleMenu");
 const hero = document.querySelector(".hero");
+const footer = document.querySelector(".footer");
+const media = document.querySelector("#media");
 
 var overlayStatus = false;
 
+// Observers
 const heroOptions = {
   rootMargin: "-96px 0px 0px 0px",
+};
+const footerOptions = {
+  rootMargin: "0px 0px -180px 0px",
 };
 
 const heroObserver = new IntersectionObserver(function (entries, heroObserver) {
@@ -22,6 +28,59 @@ const heroObserver = new IntersectionObserver(function (entries, heroObserver) {
 }, heroOptions);
 
 heroObserver.observe(hero);
+
+const mediaObserver = new IntersectionObserver(function (
+  entries,
+  mediaObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      media.style.position = "fixed";
+    } else {
+      media.style.position = "relative";
+    }
+  });
+},
+footerOptions);
+
+mediaObserver.observe(footer);
+
+// SlideShow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
 //EVENT LISTENERS
 btnHamburger.addEventListener("click", hamburger_btnClicked);
@@ -58,7 +117,7 @@ function fadeOut() {
 function window_onResized() {
   const windowWidth = window.innerWidth;
   if (windowWidth > 832 && overlay) {
-    console.log(windowWidth);
+    // console.log(windowWidth);
     fadeOut();
     header.classList.remove("open");
   }

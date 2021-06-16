d47dd18c45d9b9731aa1cd110ce462bad654b12a
F Aurelio <onceacurious@gmail.com>
2021-06-16 22:43:59 +0800

    Draft Services Section

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,25 +3,41 @@ const btnHamburger = document.querySelector("#hamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const toggler = document.getElementById("toggleMenu");
-const hero = document.querySelector(".hero");
-const footer = document.querySelector(".footer");
 const media = document.querySelector("#media");
-
+const serviceItem = document.querySelector(".item-overlay");
+const serviceImg = document.querySelector("#serviceImg");
+const serviceHeader = document.querySelector("#serviceHeader");
 
 var overlayStatus = false;
 
-// Observers
+// OBSERVERS
+var accent_primary = "#01f3e7";
+var accent_secondary = "#01f3aa";
+var white = "#ffffff";
+var dark = "#000000";
+var lightGray = "#746f6f";
+
+const hero = document.querySelector(".hero");
+const features = document.querySelector(".features");
+const footer = document.querySelector(".footer");
+
 const heroOptions = {
-  rootMargin: "-96px 0px 0px 0px",
+  rootMargin: "-64px 0px 0px 0px",
 };
+
+const featuresOptions = {
+  rootMargin: "-64px 0px 0px 0px",
+};
+
 const footerOptions = {
-  rootMargin: "0px 0px -180px 0px",
+  rootMargin: "0px 0px -300px 0px",
 };
 
 const heroObserver = new IntersectionObserver(function (entries, heroObserver) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       header.classList.add("nav-scrolled");
+      // header.style.background = accent_primary;
     } else {
       header.classList.remove("nav-scrolled");
     }
@@ -30,15 +46,24 @@ const heroObserver = new IntersectionObserver(function (entries, heroObserver) {
 
 heroObserver.observe(hero);
 
+// const featureObserver = new IntersectionObserver(function (entries, featureObserver) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       header.style.background = accent_secondary;
+//     } else{
+//       header.style.background = accent_primary;
+//     }
+//   });
+// }, featuresOptions);
+
+// featureObserver.observe(features)
+
 const mediaObserver = new IntersectionObserver(function (
   entries,
   mediaObserver
 ) {
   entries.forEach((entry) => {
-
-    
     if (!entry.isIntersecting) {
-      
       media.style.position = "fixed";
     } else {
       const navScrolled = document.querySelector(".nav-scrolled");
@@ -56,11 +81,11 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -89,9 +114,10 @@ function showSlides(n) {
 }
 
 //EVENT LISTENERS
+window.onresize = window_onResized;
 btnHamburger.addEventListener("click", hamburger_btnClicked);
 
-window.onresize = window_onResized;
+// hover(serviceImg, itemOverlay_fadeIn, itemOverlay_fadeOut);
 
 //FUNCTIONS
 function hamburger_btnClicked() {
@@ -104,7 +130,6 @@ function hamburger_btnClicked() {
     fadeIn();
     overlayStatus = true;
   }
-
 }
 
 function fadeIn() {
@@ -121,10 +146,23 @@ function fadeOut() {
 
 function window_onResized() {
   const windowWidth = window.innerWidth;
-  
+
   if (windowWidth > 892 && overlay) {
     fadeOut();
     header.classList.remove("open");
   }
+}
 
+function hover(element, enter, leave) {
+  element.addEventListener("mouseenter", enter);
+  element.addEventListener("mouseleave", leave);
+}
+
+function itemOverlay_fadeIn() {
+  serviceItem.classList.add("fade-in");
+  serviceItem.classList.remove("fade-out");
+}
+function itemOverlay_fadeOut() {
+  serviceItem.classList.remove("fade-in");
+  serviceItem.classList.add("fade-out");
 }
